7a0bc1371d8b8348f40c044108b3aeb9200cf602
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-09-16 15:51:28 +0200

    added the landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import Welcome from './landingPage/Welcome'
+
 
 function App() {
 
   return (
     <>
-     
+    <div className='landingPage'>
+    <Welcome />
+    </div>
     </>
   )
 }
