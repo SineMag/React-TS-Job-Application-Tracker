73bc30cebe0b3a5cb7843a18c115dabb5bae5f1e
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-09-16 16:55:27 +0200

    created login/register buttons for the landing page

--- src/landingPage/WelcomeImage.tsx
+++ src/landingPage/WelcomeImage.tsx
@@ -1,17 +1,21 @@
 import React from "react";
-import WelcomeImage from "../assets/image 19.png";
+import WelcomeImage from "../assets/Screenshot 2025-09-16 164150.png";
 
 export default function Welcome() {
   return (
     <div className="welcomePage">
       <div className="left">
-        <h1>Job Application Tracker</h1> <br />
+        <h4>Job Application Tracker</h4> <br />
         <h2>Welcome</h2>
-        <h3>Track your applications, measure your progress — and stay ahead in your job search.</h3>
-        <p>Please <button>Login</button> or <button>Register</button></p>
+        <h3>
+          Track your applications, measure your progress — and stay ahead in
+          your job search.
+        </h3>
+        <p>
+          Please <button className="welcomeButton">Login</button> or <button>Register</button>
+        </p>
       </div>
-            <img src={WelcomeImage} alt="Hero Landing image on the right side" />
-
+      <img src={WelcomeImage} alt="Hero Landing image on the right side" />
     </div>
   );
 }
